7191f301f26456ef67bba432e9fa1157e6577fc7
Martina <martinaescorial9@gmail.com>
2021-06-19 15:18:06 +0200

    Add project files.

---
+++ UD4E16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UD4E16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double IVA = 0.21;
+
+            String texto = "Introduce el precio de un producto";
+
+            double precio = Convert.ToDouble(texto);
+
+            double precioFinal = precio + (precio * IVA);
+
+            Console.WriteLine(precioFinal);
+        }
+    }
+}
